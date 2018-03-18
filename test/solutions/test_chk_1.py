ac61a1a08925e23a363b6de7c01eae332f90acb2
sbx_user1059 <sbx_user1059@ip-10-12-203-36.eu-west-2.compute.internal>
2018-03-18 14:45:23 +0000

    Sun Mar 18 14:45:23 UTC 2018

--- test/solutions/test_chk_1.py
+++ test/solutions/test_chk_1.py
@@ -1,2 +1,14 @@
-# -*- coding: utf-8 -*-
+import unittest
 
+from lib.solutions.checkout import checkout
+
+
+class TestCheckout(unittest.TestCase):
+    def test_checkout(self):
+        self.assertEqual(checkout("A B C D"), 115)
+        self.assertEqual(checkout("A B"), 80)
+        self.assertEqual(checkout("A F D"), -1)
+
+
+if __name__ == '__main__':
+    unittest.main()
