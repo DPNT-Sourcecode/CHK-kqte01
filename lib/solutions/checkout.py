8c36b9bd2de5585e8a1e46b082c18b382bb52b10
sbx_user1059 <sbx_user1059@ip-10-12-203-36.eu-west-2.compute.internal>
2018-03-18 14:42:23 +0000

    Sun Mar 18 14:42:23 UTC 2018

--- lib/solutions/checkout.py
+++ lib/solutions/checkout.py
@@ -1,6 +1,59 @@
+from collections import defaultdict
+
+class Supermarket:
+    
+    #Our price table and offers: 
+    #+------+-------+----------------+
+    #| Item | Price | Special offers |
+    #+------+-------+----------------+
+    #| A    | 50    | 3A for 130     |
+    #| B    | 30    | 2B for 45      |
+    #| C    | 20    |                |
+    #| D    | 15    |                |
+    #+------+-------+----------------+
+
+    def __init__(self):
+        self.prices = {
+                "A" : 50,
+                "B" : 30,
+                "C" : 20,
+                "D" : 15            
+                }
+        
+        self.offers = {
+                "A" : {3 : 130,},
+                "B" : {2 : 45}
+                }
+
+    
+    def make_offer(self, item, count):
+        self.offers.get(item, self.prices[item])
+        
+        
+    
+    def checkout(self, basket):
+        grouped = defaultdict(int)
+        for item in basket:
+            grouped[item] += 1
+    
+        price = 0
+        for item, count in grouped.iteritems():                
+#            if item in self.offers and count >= self.offers[item]:                
+#                price += make_offer(item, count)
+#            el
+            if item in self.prices:
+                price += count * self.prices[item]
+            else:
+                return -1
+
+        return price
 
 
 # noinspection PyUnusedLocal
 # skus = unicode string
 def checkout(skus):
-    raise NotImplementedError()
+    basket = skus.split()
+    shop = Supermarket()
+    return shop.checkout(basket)
+    
+    
