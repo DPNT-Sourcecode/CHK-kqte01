cafe531d86a18c2cbea7076dd35321f3e1ca7bfc
Julian Ghionoiu <iulian.ghionoiu@gmail.com>
2018-03-18 19:25:07 +0000

    Sun Mar 18 19:25:07 GMT 2018

--- lib/solutions/checkout.py
+++ lib/solutions/checkout.py
@@ -7,37 +7,37 @@ class Supermarket:
     def __init__(self, offers_file):
         self.prices = defaultdict(list)
         self.giveAway = {}
-        self.buy_any = []
+        self.buyAny = []
         
         with open(offers_file) as file_in:
-            for offer_line in file_in:
-                offer = offer_line.split()
+            for offerLine in file_in:
+                offer = offerLine.split()
                 
                 if offer[0] == "price":
                     item, count, price = offer[1:]
                     self.prices[item].append(Supermarket.Price(int(count), int(price)))
 
                 elif offer[0] == "give_away":
-                    free, required_count, item = offer[1:]
-                    self.giveAway[free] = (int(required_count), item)
+                    free, requiredCount, item = offer[1:]
+                    self.giveAway[free] = (int(requiredCount), item)
                 
                 elif offer[0] == "buy_any":
                     count, price = offer[1:3]
                     items = offer[3:] # items in descending price order
-                    self.buy_any.append((int(count), int(price), items))
+                    self.buyAny.append((int(count), int(price), items))
                     
            
             
     def remove_give_aways(self, bought):
         for free, requirement in self.giveAway.iteritems():
-            required_count, item = requirement
+            requiredCount, item = requirement
             if free in bought:
-                item_count = bought.get(item, 0)
-                free_count = item_count / required_count
-                if bought[free] < free_count:
+                itemCount = bought.get(item, 0)
+                freeCount = itemCount / requiredCount
+                if bought[free] < freeCount:
                     bought[free] = 0
                 else:
-                    bought[free] -= free_count
+                    bought[free] -= freeCount
 
     
     def checkout(self, basket):
@@ -49,19 +49,19 @@ class Supermarket:
         
         total = 0
         
-        # buy # any of        
-        for count, price, items_on_offer in self.buy_any:
+        # buy from any of group offer
+        for count, price, itemsOnOffer in self.buyAny:
             
-            any_of_group = []
-            for item in items_on_offer:
-                any_of_group += bought.pop(item, 0) * [item]
+            anyOfGroup = []
+            for item in itemsOnOffer:
+                anyOfGroup += bought.pop(item, 0) * [item]
                                 
-            total += len(any_of_group) / count * price
-            reminder = len(any_of_group) % count
+            total += len(anyOfGroup) / count * price
+            reminder = len(anyOfGroup) % count
             if reminder:
                 # put not matched back on bought list
-                not_matched = any_of_group[-reminder:]
-                for item in not_matched:
+                notMatched = anyOfGroup[-reminder:]
+                for item in notMatched:
                     bought[item] += 1
                             
     
@@ -71,8 +71,8 @@ class Supermarket:
                 return -1
             
             for offer in self.prices[item]:
-                offers_num = count / offer.count
-                price = offers_num * offer.price
+                offersNum = count / offer.count
+                price = offersNum * offer.price
                 total += price
                 
                 # reduce count by what was added to total already
