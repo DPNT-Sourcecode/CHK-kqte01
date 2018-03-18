631d6db361e250b9a5507f8d2aa77514254782eb
sbx_user1059 <sbx_user1059@ip-10-11-19-166.eu-west-2.compute.internal>
2018-03-18 17:10:05 +0000

    Sun Mar 18 17:10:05 UTC 2018

--- lib/solutions/checkout.py
+++ lib/solutions/checkout.py
@@ -33,74 +33,60 @@ class Supermarket:
     #| F    | 10    | 2F get one F free      |
     #+------+-------+------------------------+
 
-    #+------+-------+------------------------+
-    #| Item | Price | Special offers         |
-    #+------+-------+------------------------+
-    #| A    | 50    | 3A for 130, 5A for 200 |
-    #| B    | 30    | 2B for 45              |
-    #| C    | 20    |                        |
-    #| D    | 15    |                        |
-    #| E    | 40    | 2E get one B free      |
-    #| F    | 10    | 2F get one F free      |
-    #| G    | 20    |                        |
-    #| H    | 10    | 5H for 45, 10H for 80  |
-    #| I    | 35    |                        |
-    #| J    | 60    |                        |
-    #| K    | 80    | 2K for 150             |
-    #| L    | 90    |                        |
-    #| M    | 15    |                        |
-    #| N    | 40    | 3N get one M free      |
-    #| O    | 10    |                        |
-    #| P    | 50    | 5P for 200             |
-    #| Q    | 30    | 3Q for 80              |
-    #| R    | 50    | 3R get one Q free      |
-    #| S    | 30    |                        |
-    #| T    | 20    |                        |
-    #| U    | 40    | 3U get one U free      |
-    #| V    | 50    | 2V for 90, 3V for 130  |
-    #| W    | 20    |                        |
-    #| X    | 90    |                        |
-    #| Y    | 10    |                        |
-    #| Z    | 50    |                        |
-    #+------+-------+------------------------+
+
 
 
     Price = namedtuple("Price", ["count", "price"])
 
-    def __init__(self):
+    def __init__(self, offers_file):
+        self.prices = defaultdict(list)
+        self.give_away = {}
         
-        self.prices = {
-                # Last price is always for a single item with no offers
-                "A" : [ 
-                        Supermarket.Price(5, 200),
-                        Supermarket.Price(3, 130),
-                        Supermarket.Price(1, 50)
-                      ],
-                "B" : [ 
-                        Supermarket.Price(2, 45),
-                        Supermarket.Price(1, 30)     
-                      ],
-                "C" : [
-                        Supermarket.Price(1, 20)
-                      ],
-                "D" : [
-                        Supermarket.Price(1, 15)
-                      ],
-                "E" : [
-                        Supermarket.Price(1, 40)
-                      ],
-                "F" : [
-                        Supermarket.Price(1, 10)
-                      ],
-                "G" : [
-                        Supermarket.Price(1, 20)
-                      ],
-                }
+        with open(offers_file) as file_in:
+            for offer_line in file_in:
+                offer = offer_line.split()
+                
+                if offer[0] == "price":
+                    item, count, price = offer[1:]
+                    self.prices[item].append(Supermarket.Price(int(count), int(price)))
+
+                elif offer[0] == "give_away":
+                    free, required_count, item = offer[1:]
+                    self.give_away[free] = (int(required_count), item)
         
-        self.give_away = {
-                "B" : (2, "E"),
-                "F" : (3, "F")
-                }
+            
+#        self.prices = {
+#                # Last price is always for a single item with no offers
+#                "A" : [ 
+#                        Supermarket.Price(5, 200),
+#                        Supermarket.Price(3, 130),
+#                        Supermarket.Price(1, 50)
+#                      ],
+#                "B" : [ 
+#                        Supermarket.Price(2, 45),
+#                        Supermarket.Price(1, 30)     
+#                      ],
+#                "C" : [
+#                        Supermarket.Price(1, 20)
+#                      ],
+#                "D" : [
+#                        Supermarket.Price(1, 15)
+#                      ],
+#                "E" : [
+#                        Supermarket.Price(1, 40)
+#                      ],
+#                "F" : [
+#                        Supermarket.Price(1, 10)
+#                      ],
+#                "G" : [
+#                        Supermarket.Price(1, 20)
+#                      ],
+#                }
+#        
+#        self.give_away = {
+#                "B" : (2, "E"),
+#                "F" : (3, "F")
+#                }
             
     
     def checkout(self, basket):
@@ -135,7 +121,7 @@ class Supermarket:
                     
         return total
 
-shop = Supermarket()
+shop = Supermarket("offers.txt")
 
 # noinspection PyUnusedLocal
 # skus = unicode string
@@ -143,4 +129,4 @@ def checkout(skus):
     basket = list(skus)
     return shop.checkout(basket)
     
-    
+    
