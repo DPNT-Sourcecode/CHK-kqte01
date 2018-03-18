e83c97c95841eeb2e7bc82cbe00bc39aa8031395
sbx_user1059 <sbx_user1059@ip-10-12-203-36.eu-west-2.compute.internal>
2018-03-18 14:59:27 +0000

    Sun Mar 18 14:59:27 UTC 2018

--- test/solutions/test_chk_1.py
+++ test/solutions/test_chk_1.py
@@ -4,10 +4,23 @@ from lib.solutions.checkout import checkout
 
 
 class TestCheckout(unittest.TestCase):
+    
+    #Our price table and offers: 
+    #+------+-------+----------------+
+    #| Item | Price | Special offers |
+    #+------+-------+----------------+
+    #| A    | 50    | 3A for 130     |
+    #| B    | 30    | 2B for 45      |
+    #| C    | 20    |                |
+    #| D    | 15    |                |
+    #+------+-------+----------------+
+    
     def test_checkout(self):
         self.assertEqual(checkout("A B C D"), 115)
         self.assertEqual(checkout("A B"), 80)
         self.assertEqual(checkout("A F D"), -1)
+        
+        self.assertEqual(checkout("A B C A C A A D"), 265)
 
 
 if __name__ == '__main__':
