8363c07c9ea806c862c834889b5c2919cbd80325
sbx_user1059 <sbx_user1059@ip-10-11-19-166.eu-west-2.compute.internal>
2018-03-18 16:37:09 +0000

    Sun Mar 18 16:37:09 UTC 2018

--- lib/solutions/checkout.py
+++ lib/solutions/checkout.py
@@ -22,43 +22,56 @@ class Supermarket:
     #| E    | 40    | 2E get one B free      |
     #+------+-------+------------------------+
 
-    Price = namedtuple("Price", ["count", "price", "give_away"])
+    Price = namedtuple("Price", ["count", "price"])
 
     def __init__(self):
         
         self.prices = {
                 # Last price is always for a single item with no offers
                 "A" : [ 
-                        Supermarket.Price(5, 200, set()),
-                        Supermarket.Price(3, 130, set()),
-                        Supermarket.Price(1, 50, set())
+                        Supermarket.Price(5, 200),
+                        Supermarket.Price(3, 130),
+                        Supermarket.Price(1, 50)
                       ],
                 "B" : [ 
-                        Supermarket.Price(2, 45, set()),
-                        Supermarket.Price(1, 30, set())     
+                        Supermarket.Price(2, 45),
+                        Supermarket.Price(1, 30)     
                       ],
                 "C" : [
-                        Supermarket.Price(1, 20, set())
+                        Supermarket.Price(1, 20)
                       ],
                 "D" : [
-                        Supermarket.Price(1, 15, set())
+                        Supermarket.Price(1, 15)
                       ],
                 "E" : [
-                        Supermarket.Price(2, 80, set("B")),
-                        Supermarket.Price(1, 40, set())
+                        Supermarket.Price(1, 40)
                       ],
                 }
-               
+        
+        self.give_away = {
+                "B" : (2, "E")
+                }
             
     
     def checkout(self, basket):
-        grouped = defaultdict(int)
+        bought = defaultdict(int)
         for item in basket:
-            grouped[item] += 1
+            bought[item] += 1
+    
+        # deduct give aways        
+        for give_away, requirement in self.give_away.iteritems():
+            required_count, item = requirement
+            if give_away in bought:
+                item_count = bought.get(item, 0)
+                free_count = item_count / required_count
+                if bought[give_away] < free_count:
+                    bought[give_away] = 0
+                else:
+                    bought[give_away] -= free_count
+            
     
         total = 0
-        
-        for item, count in grouped.iteritems():                
+        for item, count in bought.iteritems():                
             if item not in self.prices:                
                 return -1
             
